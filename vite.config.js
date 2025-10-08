b348723d17f07f63ce00e3ee930e835d9b18f259
Kuldeepkant26 <picasuplayers@gmail.com>
2025-10-08 10:25:25 +0530

    Updated Dockerfile, App.jsx, and vite.config.js from EC2

--- vite.config.js
+++ vite.config.js
@@ -3,20 +3,21 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
   server: {
-    host: '0.0.0.0',
-    allowedHosts: [
-      'www.naturallygood.in', 
-      'naturallygood.in',
-      '13.235.113.220'  // Add your EC2 IP
-    ]
-  }
+    host: true, // bind 0.0.0.0 in Docker
+    allowedHosts: ['naturallygood.in', 'www.naturallygood.in'],
+    hmr: {
+      protocol: 'wss',
+      host: 'www.naturallygood.in', // or 'naturallygood.in'
+      clientPort: 443,
+    },
+  },
 })
+
